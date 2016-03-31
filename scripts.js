de5695bd312b39e15d169d1c6c69876b05cc91e2
nickshevchenko <nick.shevchenko@yahoo.co.uk>
2016-04-01 01:59:54 +0300

    jquery for profile data

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-for (var i = 1; i < 13; i++) {
+profileContentStart = '\
+<div class="picture-profile">';
+profilePicture = '</div>\
+<div class="pokemon-name-profile">';
+profileContentEnd = '\
+</div>\
+<div class="abilities-profile"></div>';
+
+for(var i = 1; i < 13; i++) {
   $.get("http://pokeapi.co/api/v2/pokemon/" +i, function (data){
     $("#pokemon-" + (data.id)).html(data.name);
-    for (var y = 0; y < data.types.length;  y++) {
+    $("#pokemon-" + (data.id)).siblings(".hidden-info").html(profileContentStart + data.name + profileContentEnd);
+    $("#pokemon-" + (data.id)).append('<input type="hidden" value="'+ data.id +'">');
+    for(var y = 0; y < data.types.length;  y++) {
       data.types[y].type.name
       prefContent = $("#ability-" + (data.id)).html()
       typeContent = prefContent + "<div class=\""+ data.types[y].type.name +" type\">"+ data.types[y].type.name +"</div>"
@@ -9,12 +19,18 @@ for (var i = 1; i < 13; i++) {
     }
   });
 };
-//div appearance
+
 $(function(){
-  $("#formfield").click(function(){
-    div = document.createElement('div');
-    $(div).addClass("profile").html();
-    $("#character").append(div);
-    $("#character").css('display','block');
-  });
+var previous = "none";
+$(".form").click(function(e){
+	current = $(e.target).parents(".form").children(".pokemon-name").children("input").val();
+	$("#character").html($(e.target).parents(".form").children(".hidden-info").html());
+    if(previous == current){
+        $("#character").css('display','none');
+        previous = "none";
+    }else{
+        $("#character").css('display','block');
+        previous = current;
+    }
+});
 });
