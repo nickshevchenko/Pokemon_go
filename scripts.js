47d67fc093d1edb009c9439ac87a066137a8d642
nickshevchenko <nick.shevchenko@yahoo.co.uk>
2016-04-02 17:15:51 +0300

    adding a new loop

--- scripts.js
+++ scripts.js
@@ -2,13 +2,18 @@ profileContentStart = '\<div class="picture-profile"><img src="';
 profileName = '"></div>\<div class="pokemon-name-profile">';
 profileAbilities = '\</div>\<div class="abilities-profile">';
 profileContentEnd = '</div>';
+
+Etypes = function(d){
+return "poison, grass";
+for (var z = 0; z < data.types.length; z++) {
+data.types[z].type.name
+};
+};
 for(var i = 1; i < 13; i++) {
   $.get("http://pokeapi.co/api/v2/pokemon/" +i, function (data){
-  	for (var z = 0; z < data.types.length; z++) {
-  		data.types[z].type.name
   		$("#pokemon-" + (data.id)).html(data.name);
     	$("#pokemon-" + (data.id)).siblings(".hidden-info").html(profileContentStart + data.sprites.front_default + profileName + data.name + profileAbilities
-    	+ data.types[z].type.name + profileContentEnd);
+    	+ Etypes(data) + profileContentEnd);
     	$("#pokemon-" + (data.id)).append('<input type="hidden" value="'+ data.id +'">');	
   	}
     for(var y = 0; y < data.types.length; y++) {
@@ -19,7 +24,6 @@ for(var i = 1; i < 13; i++) {
     }
   });
 };
-
 $(function(){
 var previous = "none";
 $(".form").click(function(e){
