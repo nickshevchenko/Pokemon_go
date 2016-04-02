d27c759db73a6e2cdd2e92f3c4d60d2e14f3c4c0
nickshevchenko <nick.shevchenko@yahoo.co.uk>
2016-04-02 18:19:37 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,15 +15,14 @@ for(var i = 1; i < 13; i++) {
     $("#pokemon-" + (data.id)).siblings(".hidden-info").html(profileContentStart + data.sprites.front_default + profileName + data.name + profileAbilities
     + Etypes(data) + profileContentEnd);
     $("#pokemon-" + (data.id)).append('<input type="hidden" value="'+ data.id +'">');	
-  });
+  };
     for(var y = 0; y < data.types.length; y++) {
       data.types[y].type.name
       prefContent = $("#ability-" + (data.id)).html()
       typeContent = prefContent + "<div class=\""+ data.types[y].type.name +" type\">"+ data.types[y].type.name +"</div>"
       $("#ability-" + (data.id)).html(typeContent);
-    };
+    });
 };
-
 $(function(){
 var previous = "none";
 $(".form").click(function(e){
