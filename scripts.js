943f25d6b0eacb87ff817913e34b9403d1742a3e
nickshevchenko <nick.shevchenko@yahoo.co.uk>
2016-04-02 17:23:57 +0300

    js changes

--- scripts.js
+++ scripts.js
@@ -11,18 +11,17 @@ data.types[z].type.name
 };
 for(var i = 1; i < 13; i++) {
   $.get("http://pokeapi.co/api/v2/pokemon/" +i, function (data){
-  		$("#pokemon-" + (data.id)).html(data.name);
-    	$("#pokemon-" + (data.id)).siblings(".hidden-info").html(profileContentStart + data.sprites.front_default + profileName + data.name + profileAbilities
-    	+ Etypes(data) + profileContentEnd);
-    	$("#pokemon-" + (data.id)).append('<input type="hidden" value="'+ data.id +'">');	
-  	}
+    $("#pokemon-" + (data.id)).html(data.name);
+    $("#pokemon-" + (data.id)).siblings(".hidden-info").html(profileContentStart + data.sprites.front_default + profileName + data.name + profileAbilities
+    + Etypes(data) + profileContentEnd);
+    $("#pokemon-" + (data.id)).append('<input type="hidden" value="'+ data.id +'">');	
+  });
     for(var y = 0; y < data.types.length; y++) {
       data.types[y].type.name
       prefContent = $("#ability-" + (data.id)).html()
       typeContent = prefContent + "<div class=\""+ data.types[y].type.name +" type\">"+ data.types[y].type.name +"</div>"
       $("#ability-" + (data.id)).html(typeContent);
-    }
-  });
+    };
 };
 $(function(){
 var previous = "none";
